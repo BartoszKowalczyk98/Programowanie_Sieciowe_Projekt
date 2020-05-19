0447a572a2fdc398e580af0e845c7bf35f91ac11
Kowalczyk.Bartosz <21577@edu.p.lodz.pl>
2020-05-19 14:01:34 +0200

    UnknownCommandException implementation
    Signed-off-by: Kowalczyk.Bartosz <21577@edu.p.lodz.pl>

--- src/Exceptions/UnknownCommandException.java
+++ src/Exceptions/UnknownCommandException.java
@@ -1,4 +1,15 @@
 package Exceptions;
 
-public class UnknownCommandException {
+public class UnknownCommandException extends Exception{
+	public UnknownCommandException() {
+	}
+
+	public UnknownCommandException(String message) {
+		super(message);
+	}
+
+	@Override
+	public String getMessage() {
+		return "Server Encountered unknown command during handling of client!";
+	}
 }
